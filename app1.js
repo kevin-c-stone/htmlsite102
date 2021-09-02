48889a1b989deeebc052aad82bfedd17d15471f0
Kevin C Stone <kevin.c.stone@outlook.com>
2021-09-01 21:23:29 -0700

    completed lab 6 updates with JS

---
+++ app1.js
@@ -0,0 +1,7 @@
+let userName = prompt('Please enter your name');
+
+if (userName == 'Kevin'){
+    document.write('Welcome Omniscient Footballer, ' + userName);
+} else {
+    document.write('Welcome ' + userName)
+}
---
+++ app3.js
@@ -0,0 +1,19 @@
+function functionToExecute () {
+    alert('OF COURSE THEY AREEEEEEEEEE!!!!!')
+}
+
+function functionToExecute1 () {
+    alert('Home is where the food is <3')
+}
+
+function functionToExecute2 () {
+    alert('The National Football League is football for the Nation')
+}
+
+function functionToExecute3 () {
+    alert('National Football Conference')
+}
+
+function functionToExecute4 () {
+    alert('American Football Conference')
+}
